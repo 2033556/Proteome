d8f38b44ba1f21080160e18acfab25c49a443212
2032708 <2032708@etu.cegepjonquiere.ca>
2021-03-31 16:47:45 -0400

    Fin du cours

---
+++ Proteome/src/TestProteine.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestProteine {
+
+    @Test
+    void testProteine(){
+        Proteome proteome = new Proteome();
+
+        Proteines cochon = new Proteines("cochon");
+        Proteines vache = new Proteines("vache");
+        Proteines tulipe = new Proteines("tulipe");
+        Proteines pomme = new Proteines("pomme");
+        Proteines humain = new Proteines("humain");
+
+        proteome.ajouterProteine(cochon);
+        proteome.ajouterProteine(vache);
+        proteome.ajouterProteine(tulipe);
+        proteome.ajouterProteine(pomme);
+        proteome.ajouterProteine(humain);
+
+        assertEquals(cochon, proteome.rechercherNomCourtExact("cochon"));
+        assertEquals(null, proteome.rechercherNomCourtExact("patate"));
+        assertEquals(vache, proteome.rechercherNomCourtExact("vache"));
+        assertEquals(tulipe,proteome.rechercherNomCourtExact("tulipe"));
+        assertEquals(pomme,proteome.rechercherNomCourtExact("pomme"));
+        assertEquals(null, proteome.rechercherNomCourtExact("ecureuil"));
+        assertEquals(humain,proteome.rechercherNomCourtExact("humain"));
+    }
+}

8abd6ccec52aabcb216baab5eff52b1e16e3dd4d
2032708 <2032708@etu.cegepjonquiere.ca>
2021-04-14 15:54:08 -0400

    Debut main

--- Proteome/src/Main.java
+++ Proteome/src/Main.java
@@ -9,47 +9,50 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         char choix;
         String chaine1;
-        String chaine2;
+        Proteines chaine2;
 
-        System.out.println("Bienvenue à protéome!");
-        System.out.println("=============================================================");
-        System.out.println("[E]ntrer des acides aminés");
-        System.out.println("[R]echercher une protéine");
-        System.out.println("[O]btenir la liste des acides aminés");
-        System.out.println("[Q]uitter");
-        choix = sc.next().toUpperCase().charAt(0);
-
-        switch (choix){
-
-            case 'E':
-                System.out.println("Entrez une chaîne représentant une liste d’acides aminés");
-                chaine1 = sc.next().toUpperCase();
-
-                System.out.println(AcideAmines.lireAcidesAmines(chaine1));
-
-                break;
-
-            case 'R':
-                System.out.println("Entrez une chaîne de charactère représentant une protéine");
-                chaine2 = sc.nextLine();
-
-                Proteome proteome = new Proteome();
+        do {
+            System.out.println("=============================================================");
+            System.out.println("Bienvenue à protéome!");
+            System.out.println("=============================================================");
+            System.out.println("[E]ntrer des acides aminés");
+            System.out.println("[R]echercher une protéine");
+            System.out.println("[O]btenir la liste des acides aminés");
+            System.out.println("[Q]uitter");
+            choix = sc.next().toUpperCase().charAt(0);
 
 
-                break;
+            switch (choix) {
 
-            case 'O':
+                case 'E':
+                    System.out.println("Entrez une chaîne représentant une liste d’acides aminés");
+                    chaine1 = sc.next().toUpperCase();
+
+                    System.out.println(AcideAmines.lireAcidesAmines(chaine1));
+
+                    break;
+
+                case 'R':
+                    System.out.println("Entrez une chaîne de charactère représentant une protéine");
+
+                    Proteome proteome = new Proteome();
+
+                    break;
+
+                case 'O':
 
 
-                break;
+                    break;
 
-            case 'Q':
+                case 'Q':
 
-                break;
+                    break;
 
-            default:
-                System.out.println("Choix introuvable");
+                default:
+                    System.out.println("Choix introuvable");
+            }
         }
+        while (choix != 'Q');
 
         System.out.println("Merci d'Avoir utilisé Protéome");
         sc.close();

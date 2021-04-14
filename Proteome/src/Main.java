d36884bdafee37e0c5689e8eac508bdc59e6d0da
2032708 <2032708@etu.cegepjonquiere.ca>
2021-04-14 15:32:43 -0400

    Debut main

--- Proteome/src/Main.java
+++ Proteome/src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.SQLOutput;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +8,11 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         char choix;
+        String chaine1;
+        String chaine2;
 
+        System.out.println("Bienvenue à protéome!");
+        System.out.println("=============================================================");
         System.out.println("[E]ntrer des acides aminés");
         System.out.println("[R]echercher une protéine");
         System.out.println("[O]btenir la liste des acides aminés");
@@ -18,11 +23,17 @@ public class Main {
 
             case 'E':
                 System.out.println("Entrez une chaîne représentant une liste d’acides aminés");
+                chaine1 = sc.next().toUpperCase();
+
+                System.out.println(AcideAmines.lireAcidesAmines(chaine1));
 
                 break;
 
             case 'R':
                 System.out.println("Entrez une chaîne de charactère représentant une protéine");
+                chaine2 = sc.nextLine();
+
+                Proteome proteome = new Proteome();
 
 
                 break;
@@ -33,7 +44,6 @@ public class Main {
                 break;
 
             case 'Q':
-                sc.close();
 
                 break;
 
@@ -41,6 +51,7 @@ public class Main {
                 System.out.println("Choix introuvable");
         }
 
+        System.out.println("Merci d'Avoir utilisé Protéome");
         sc.close();
     }
 }
